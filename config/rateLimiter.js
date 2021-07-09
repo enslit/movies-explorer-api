6a8e6ee5cd182f1f3284815009941b2d1c12bcaa
Aleksandr <alex@enslit.ru>
2021-07-09 07:23:51 +0300

    build: Время в мс вынес в константу

--- config/rateLimiter.js
+++ config/rateLimiter.js
@@ -1,6 +1,8 @@
+import { TIME_MS } from './constants';
+
 const rateLimit = require('express-rate-limit');
 
 module.exports.limiter = rateLimit({
-  windowMs: 15 * 60 * 1000,
+  windowMs: 15 * TIME_MS.minute,
   max: 50,
 });
